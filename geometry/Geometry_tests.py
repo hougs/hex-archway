8178fff9859c3bae53a74b6620c34016063e7ae9
Juliet Hougland <juliet@deepsignal.com>
2014-05-07 00:02:48 -0700

    Adds tests and more properties to vertes positioner

--- geometry/Geometry_tests.py
+++ geometry/Geometry_tests.py
@@ -1,11 +1,29 @@
 from VertexPositioner import Hexagon
+from VertexPositioner import VertexPositioner
 import numpy.testing as nptst
+import itertools as it
 
 class Geometry_tests():
     def hexagon_test(self):
         hexagon = Hexagon(1, 0, 0)
-        print "vertices: " + str(hexagon.vertices)
-        nptst.assert_almost_equal(hexagon.vertices[0, 0], 0)
-        nptst.assert_almost_equal(hexagon.vertices[0, 1], 1)
-        nptst.assert_almost_equal(hexagon.vertices[3, 0], 0)
-        nptst.assert_almost_equal(hexagon.vertices[3, 1], -1)
+        nptst.assert_almost_equal(hexagon.vertices[0][0], 0)
+        nptst.assert_almost_equal(hexagon.vertices[0][1], 1)
+        nptst.assert_almost_equal(hexagon.vertices[3][0], 0)
+        nptst.assert_almost_equal(hexagon.vertices[3][1], -1)
+
+    def flat_hexagon_test(self):
+        hex2by2 = VertexPositioner(1, 2, 2)
+        actual_2by2_hexagons = hex2by2.flat_hexagons
+        expected_2by2_hexagons = [Hexagon(1, x_center, y_center) for
+                                  x_center,  y_center in it.product([-1, 1],
+                                                                    [-1, 1])]
+        print "x: " + str([hex.__dict__ for hex in actual_2by2_hexagons])
+        for actual_hexagon, expected_hexagon in zip(actual_2by2_hexagons,
+                                 expected_2by2_hexagons):
+            assert actual_hexagon == expected_hexagon
+
+    def flat_hex_position_test(self):
+        actual_1by1_hexs = VertexPositioner._flat_hex_pos(1, 1, 1)
+        expected_1by1_hexs = [Hexagon(1, 0, 0)]
+        actual_2by2_hexs = VertexPositioner._flat_hex_pos(1, 2, 2)
+        expected_2by2_hexs = [Hexagon(1,)]
--- geometry/VertexPositioner.py
+++ geometry/VertexPositioner.py
@@ -1,19 +1,25 @@
 import numpy as np
 
-
 class Hexagon():
     def __init__(self, side_length, center_x, center_y):
+        self.center_x = center_x
+        self.center_y = center_y
         self.vertices = self._make_vertices(side_length, center_x, center_y)
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return all(self.__dict__ == other.__dict__)
+        else:
+            return False
 
     @staticmethod
     def _make_vertices(side_length, center_x, center_y):
-        vertices = np.empty([6, 2])
+        vertices = []
         for vertex_number in range(0, 5, 1):
             angle = (vertex_number * np.pi)/3
             x_position = side_length * np.sin(angle)
             y_position = side_length * np.cos(angle)
-            vertices[vertex_number, :] = [x_position + center_x,
-                                           y_position + center_y]
+            vertices.append((x_position + center_x, y_position + center_y))
         return vertices
 
 class VertexPositioner():
@@ -32,7 +38,8 @@ class VertexPositioner():
         self.flat_hexagons = self._flat_hex_pos(approx_hex_side_length,
                                             n_hex_x_rows,
                                            n_hex_y_rows)
-        self.vertex_positions = self._rect_to_cyl()
+        #self.arc_length = np.pi *
+        #self.vertex_positions = self._rect_to_cyl()
 
     @staticmethod
     def _flat_hex_pos(hex_side_length, n_hex_x_row, n_hex_y_row):
@@ -55,8 +62,9 @@ class VertexPositioner():
         seed_pos_x = hex_side_length * (n_hex_x_row - 1)
         seed_pos_y = hex_side_length * (n_hex_y_row - 1)
         hexagons = []
-        for center_x in range(-seed_pos_x, seed_pos_x, hex_side_length):
-            for center_y in range(-seed_pos_y, seed_pos_y, hex_side_length):
+        for center_x in range(-seed_pos_x, seed_pos_x, 2 * hex_side_length):
+            for center_y in range(-seed_pos_y, seed_pos_y,
+                                  2 * hex_side_length):
                 hexagons.append(Hexagon(hex_side_length, center_x, center_y))
         return hexagons
 
